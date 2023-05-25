85d3497207c9e71467b70f046cc4bc779e913dac
dt <1041727846@qq.com>
2023-05-25 18:06:43 +0800

    init

---
+++ DataBase/DbDto/UserBasicInfoMDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.DbDto
+{
+    public class UserBasicInfoMDto
+    {
+        public int user_seq;
+        public string login_id;
+        public string password;
+        public string user_name;
+        public string user_kana_name;
+        public string user_display_name;
+        public string email_address;
+        public string mayaku_license_no;
+        public string memo;
+        public string user_image_file;
+        public string valid_period_start_date;
+        public string valid_period_end_date;
+        public string renzoku_ninshou_failure_kaisuu;
+        public string login_kaisuu;
+        public string zenkai_login_date_time;
+        public string zenkai_password_change_date_time;
+        public string is_deleted;
+        public string created_by;
+        public DateTime created_at;
+        public string updated_by;
+        public DateTime updated_at;
+    }
+}
---
+++ DataBase/Service/BaseService.cs
@@ -0,0 +1,47 @@
+﻿using DataBase.DbDto;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBase.Service
+{
+    public class BaseService<T>
+    {
+        public static List<T> GetAllData()
+        {
+            List<T> list = new List<T>();
+
+            PropertyInfo[] infos = typeof(T).GetProperties();
+
+            string connStr = "server=192.168.0.169;username=postgres;password=postgres;database=APPLICATION";
+            NpgsqlConnection connection = new NpgsqlConnection(connStr);
+            try
+            {
+                connection.Open();
+                string commandStr = "select * from g200010.user_basic_info_m";
+                NpgsqlDataAdapter adapter = new NpgsqlDataAdapter(commandStr, connection);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+            }
+            finally
+            {
+                connection.Close();
+            }
+
+            if ("之前听泡泡老师说伍泽凯小朋友上课表现非常好，上完后发现伍泽凯小朋友上课表现确实挺不错的，遵守课堂纪律，而且组装机器人的速度也非常快，继续保持就好".Equals("#"))
+            {
+                if(list.Count > 0)
+                {
+                    Console.WriteLine("这样的话就是很好了");
+                }
+            }
+
+            return list;
+        }
+    }
+}
