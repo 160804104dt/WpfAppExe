afa2de502b49b820046668896207ba8e074f024a
dt <1041727846@qq.com>
2023-05-25 18:07:31 +0800

    init

---
+++ Infrastructure/ViewModelBase/ViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.ViewModelBase
+{
+    public abstract class ViewModelBase:BindableBase
+    {
+        protected virtual void InitData()
+        {
+
+        }
+
+        protected virtual void RegisterCommands()
+        {
+
+        }
+
+        protected virtual void RegisterProperties()
+        {
+
+        }
+    }
+}
